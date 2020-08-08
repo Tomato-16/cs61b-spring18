a423e14ce073be41404e5b91b7370ecfbda20965
Tomato-16 <2975295212@qq.com>
2020-08-08 20:43:08 +0800

    Finish the method of calculating distances

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
   public double yyVel;
   public double mass;
   public String imgFileName;
-  
+
   /** Contructor method of class Planet */
   public Planet(double xP, double yP, double xV,
               double yV, double m, String img) {
@@ -17,5 +17,10 @@ public class Planet {
       xxPos = p.xxPos; yyPos = p.yyPos;
       xxVel = p.xxVel; yyVel = p.yyVel;
       mass = p.mass;   imgFileName = p.imgFileName;
+  }
+  /** calculate the distance between this Planet and the other Planet */
+  public double calcDistance(Planet other) {
+      return Math.sqrt((other.xxPos - this.xxPos)*(other.xxPos - this.xxPos) +
+                       (other.yyPos - this.yyPos)*(other.yyPos - this.yyPos));
   }
 }
