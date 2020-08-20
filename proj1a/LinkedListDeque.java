4764c3bb43016541c379c8730351116c1d14016b
Tomato-16 <2975295212@qq.com>
2020-08-20 16:17:41 +0800

    Completed LinkedListDeque, given tests passed, waited for more tests

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -14,19 +14,24 @@ public class LinkedListDeque<T> {
     }
 
     /* create an empty linked list deque */
-    /* I am having a problem here!!!
     public LinkedListDeque() {
         size = 0;
-        sentinel = new Node(null, )
-    } */
+        sentinel = new Node(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
     /* add item to the first and last of the Deque */
     public void addFirst(T item) {
         size += 1;
+        Node originFirst = sentinel.next;
         sentinel.next = new Node(item, sentinel, sentinel.next);
+        originFirst.prev = sentinel.next;
     }
     public void addLast(T item) {
         size += 1;
+        Node originLast = sentinel.prev;
         sentinel.prev = new Node(item, sentinel.prev, sentinel);
+        originLast.next = sentinel.prev;
     }
     /* test if the Deque is empty */
     public boolean isEmpty() {
@@ -35,23 +40,31 @@ public class LinkedListDeque<T> {
     public int size() {
         return size;
     }
+    /* print the items in the deque, separated by a space */
     public void printDeque() {
-
+        Node ptr = sentinel.next;
+        while (ptr != sentinel) {
+            System.out.print(ptr.item + " ");
+            ptr = ptr.next;
+        }
+        System.out.println();
     }
     /* remove the item from the first and last of the Deque */
     public T removeFirst() {
-        Node temp = sentinel.next;
-        sentinel.next = sentinel.next.next;
-        sentinel.next.prev = sentinel;
-        return temp.item;
+        size -= 1;
+        Node originFirst = sentinel.next;
+        sentinel.next = originFirst.next;
+        originFirst.next.prev = sentinel;
+        return originFirst.item;
     }
     public T removeLast() {
-        Node temp = sentinel.prev;
-        sentinel.prev = sentinel.prev.prev;
-        sentinel.prev.next = sentinel;
-        return temp.item;
+        size -= 1;
+        Node originLast = sentinel.prev;
+        sentinel.prev = originLast.prev;
+        originLast.prev.next = sentinel;
+        return originLast.item;
     }
-    /* get the item specified by index */
+    /* get the item specified by index, iteratively */
     public T get(int index) {
         Node ptr = sentinel.next;
         while (index != 0) {
@@ -60,9 +73,16 @@ public class LinkedListDeque<T> {
         }
         return ptr.item;
     }
-    /*
+    /* get the item specified by index, recursively */
     public T getRecursive(int index) {
-
+        return getHelper(sentinel.next, index);
     }
-     */
+    /* private helper method for recursive implementation */
+    private T getHelper(Node current, int indices) {
+        if (indices == 0) {
+            return current.item;
+        } else {
+         return getHelper(current.next, indices - 1);
+        }
+    }
 }
