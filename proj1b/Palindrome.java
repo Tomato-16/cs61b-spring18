740b37965258be75da0e20758d9e28f54a3b5a9e
Tomato-16 <2975295212@qq.com>
2020-09-14 17:17:24 +0800

    complete Task3B, using the recursive way!

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -10,6 +10,16 @@ public class Palindrome {
 
     /* */
     public boolean isPalindrome(String word) {
-        return true;
+        return palindromeHelper(wordToDeque(word));
+    }
+    /* private helper method to determine if a deque of character is palindrome */
+    private boolean palindromeHelper(Deque<Character> deque) {
+        if (deque.size() == 1 || deque.size() == 0) {
+            return true;
+        } else if (deque.removeFirst() == deque.removeLast()){
+            return palindromeHelper(deque);
+        } else {
+            return false;
+        }
     }
 }
