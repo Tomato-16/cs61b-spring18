5543938770d2eab80960e1fc816641422b0f7968
Tomato-16 <2975295212@qq.com>
2020-11-29 11:54:37 +0800

    finish Percolation.java, backwash to be solved

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -6,9 +6,14 @@ public class Percolation {
     private WeightedQuickUnionUF weightedQuickUnionUF;
     private boolean[][] siteStatus;
     private int length;
-    private int upSite;
-    private int downSite;
+    private int topSite;
+    private int bottomSite;
     private int numOfOpen;
+
+    // private helper method used to convert the ideal indices in matrix to actual indices in UF
+    private int UFNumber(int row, int col) {
+        return row * length + col;
+    }
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
@@ -18,39 +23,77 @@ public class Percolation {
         weightedQuickUnionUF = new WeightedQuickUnionUF(N*N + 2);
         siteStatus = new boolean[N][N];
         length = N;
-        upSite = N*N;
-        downSite = N*N + 1;
+        topSite = N*N;
+        bottomSite = N*N + 1;
         numOfOpen = 0;
     }
 
-    // private helper method used to convert the ideal indices in matrix to actual indices in UF
-    private int UFNumber(int row, int col) {
-        return row * length + col;
-    }
-/*
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (siteStatus[row][col]) {
-            return;
-        } else {
+        if (row < 0 || row > length - 1 || col < 0 || col > length - 1) {
+            throw new IndexOutOfBoundsException("row and col must between 0 and N-1");
+        }
+        if (!siteStatus[row][col]) {
             // open this site
             siteStatus[row][col] = true;
             // check the surrounding sites and connect each other
-
+            connectSurroundOpen(row, col);
             // update numOfOpen
             numOfOpen += 1;
         }
     }
-*/
+
+    // private helper method used to connect surrounding open sites
+    // no need to BFS search through the array!!
+    private void connectSurroundOpen(int row, int col) {
+        int thisSite = UFNumber(row, col);
+        // check upSite
+        if (row == 0) {
+            weightedQuickUnionUF.union(thisSite, topSite);
+        } else if (siteStatus[row-1][col]){
+            int upSite = UFNumber(row-1, col);
+            weightedQuickUnionUF.union(thisSite, upSite);
+        }
+        // check downSite
+        if (row == length - 1) {
+            weightedQuickUnionUF.union(thisSite, bottomSite);
+        } else if (siteStatus[row+1][col]) {
+            int downSite = UFNumber(row+1, col);
+            weightedQuickUnionUF.union(thisSite, downSite);
+        }
+        // check leftSite
+        if (col == 0) {
+            ;
+        } else if (siteStatus[row][col - 1]) {
+            int leftSite = UFNumber(row, col-1);
+            weightedQuickUnionUF.union(thisSite, leftSite);
+        }
+        // check rightSite
+        if (col == length - 1) {
+            ;
+        } else if (siteStatus[row][col+1]) {
+            int rightSite = UFNumber(row, col+1);
+            weightedQuickUnionUF.union(thisSite, rightSite);
+        }
+    }
+
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return siteStatus[row][col];
+        if (row >= 0 && row <= length - 1 && col >= 0 && col <= length - 1) {
+            return siteStatus[row][col];
+        } else {
+            throw new IndexOutOfBoundsException("row and col must between 0 and N-1");
+        }
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        int thisSite = UFNumber(row, col);
-        return weightedQuickUnionUF.connected(thisSite, upSite);
+        if (row >= 0 && row <= length - 1 && col >= 0 && col <= length - 1) {
+            int thisSite = UFNumber(row, col);
+            return weightedQuickUnionUF.connected(thisSite, topSite);
+        } else {
+            throw new IndexOutOfBoundsException("row and col must between 0 and N-1");
+        }
     }
 
     // number of open sites
@@ -60,7 +103,7 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return weightedQuickUnionUF.connected(upSite, downSite);
+        return weightedQuickUnionUF.connected(topSite, bottomSite);
     }
 
     // use for unit testing (not required)
